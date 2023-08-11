bb42c0f02ddef085d04141eb7c798007d03f99a5
Daniil Naumetc <daniil.naumetc@gmail.com>
2023-08-11 14:59:46 +0200

    seed in pic

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
-use std::{u8, marker::PhantomData, path::PathBuf, str::FromStr};
+use std::{marker::PhantomData, path::PathBuf, str::FromStr, u8};
 
 use clap::Parser;
 use image::{ImageBuffer, Pixel, Primitive, Rgb};
-use rand::{prelude::*, distributions};
-
+use rand::SeedableRng;
+use rand::{distributions, prelude::*};
 
 struct PixelImage<T> {
-    width: u32,
-    height: u32,
+    width: u64,
+    height: u64,
     // image::Rgb<u8>
     pixels: Vec<T>,
 }
 
 impl PixelImage<Rgb<u8>> {
-
-    fn new(width: u32, height: u32, rgb: Option<Rgb<u8>>) -> Self {
+    fn new(width: u64, height: u64, rgb: Option<Rgb<u8>>) -> Self {
         let pixel = rgb.unwrap_or(Rgb([255, 255, 255]));
         let mut pixels: Vec<Rgb<u8>> = Vec::with_capacity((width * height) as usize);
         for _ in 0..width * height {
             pixels.push(rgb.unwrap_or(pixel));
         }
-        println!("{}", pixels.len());
         Self {
             width,
             height,
@@ -28,7 +26,7 @@ impl PixelImage<Rgb<u8>> {
         }
     }
 
-    fn get_pixel(&self, x: u32, y: u32) -> Option<&Rgb<u8>> {
+    fn get_pixel(&self, x: u64, y: u64) -> Option<&Rgb<u8>> {
         if x < self.width && y < self.height {
             Some(&self.pixels[(y * self.width + x) as usize])
         } else {
@@ -36,26 +34,30 @@ impl PixelImage<Rgb<u8>> {
         }
     }
 
-    fn set_pixel(&mut self, x: u32, y: u32, pixel: Rgb<u8>) {
+    fn set_pixel(&mut self, x: u64, y: u64, pixel: Rgb<u8>) {
         if x < self.width && y < self.height {
             self.pixels[(y * self.width + x) as usize] = pixel.into();
         }
     }
 
     fn export_image(&self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-        let pixel_data = self.pixels.iter().flat_map(|p| p.channels().to_vec()).collect::<Vec<u8>>();
-        ImageBuffer::from_raw(self.width, self.height, pixel_data).unwrap()
+        let pixel_data = self
+            .pixels
+            .iter()
+            .flat_map(|p| p.channels().to_vec())
+            .collect::<Vec<u8>>();
+        ImageBuffer::from_raw(self.width as u32, self.height as u32, pixel_data).unwrap()
     }
 
-    fn upscale(&self, factor: u32) -> Self {
+    fn upscale(&self, factor: u64) -> Self {
         let mut new_image = Self::new(self.width * factor, self.height * factor, None);
         for x in 0..self.height {
             for y in 0..self.width {
                 let pixel = self.get_pixel(x, y).unwrap();
-                
+
                 for ix in 0..factor {
                     for iy in 0..factor {
-                        new_image.set_pixel(x*factor + ix, y*factor + iy, pixel.clone());
+                        new_image.set_pixel(x * factor + ix, y * factor + iy, pixel.clone());
                     }
                 }
             }
@@ -66,22 +68,43 @@ impl PixelImage<Rgb<u8>> {
 }
 
 struct Drawyer<T> {
-    cursor_x: u32,
-    cursor_y: u32,
+    cursor_x: u64,
+    cursor_y: u64,
     image: PixelImage<T>,
+
+    rng: StdRng,
 }
 
 impl Drawyer<Rgb<u8>> {
-    fn new(width: u32, height: u32) -> Self {
+    fn new(width: u64, height: u64, rng: StdRng) -> Self {
         Self {
             cursor_x: 0,
             cursor_y: 0,
             image: PixelImage::new(width, height, None),
+            rng,
+        }
+    }
+
+    fn with_seed(width: u64, height: u64, seed: u64) -> Self {
+        let mut rng = StdRng::seed_from_u64(seed);
+        Self::new(width, height, rng)
+    }
+
+    fn rng(&mut self) -> &mut StdRng {
+        &mut self.rng
+    }
+
+    fn with_image(rng: StdRng, image: PixelImage<Rgb<u8>>) -> Self {
+        Self {
+            cursor_x: 0,
+            cursor_y: 0,
+            image,
+            rng,
         }
     }
 
     fn random_cursor(&mut self) {
-        let mut rng = rand::thread_rng();
+        let rng = &mut self.rng;
         // self.cursor_x = rng.gen_range(0..self.image.width);
         // self.cursor_y = rng.gen_range(0..self.image.height);
         let quarter = self.image.width / 4;
@@ -93,7 +116,7 @@ impl Drawyer<Rgb<u8>> {
         self.image.set_pixel(self.cursor_x, self.cursor_y, pixel);
     }
 
-    fn move_cursor(&mut self, x: u32, y: u32) {
+    fn move_cursor(&mut self, x: u64, y: u64) {
         self.cursor_x = x;
         self.cursor_y = y;
     }
@@ -102,35 +125,38 @@ impl Drawyer<Rgb<u8>> {
         let x = self.cursor_x as i32 + x;
         let y = self.cursor_y as i32 + y;
         if x >= 0 && y >= 0 {
-            self.cursor_x = x as u32;
-            self.cursor_y = y as u32;
+            self.cursor_x = x as u64;
+            self.cursor_y = y as u64;
         }
     }
 
-    fn distance_from_top(&self) -> u32 {
+    fn distance_from_top(&self) -> u64 {
         self.cursor_y
     }
 
-    fn distance_from_bottom(&self) -> u32 {
+    fn distance_from_bottom(&self) -> u64 {
         self.image.height - self.cursor_y
     }
 
-    fn distance_from_left(&self) -> u32 {
+    fn distance_from_left(&self) -> u64 {
         self.cursor_x
     }
 
-    fn distance_from_right(&self) -> u32 {
+    fn distance_from_right(&self) -> u64 {
         if self.cursor_x > self.image.width {
-            println!("Cursor out of bounds {} > {}", self.cursor_x, self.image.width);
+            println!(
+                "Cursor out of bounds {} > {}",
+                self.cursor_x, self.image.width
+            );
         }
         self.image.width - self.cursor_x
     }
 }
 
-
-
-struct Artist <T> {
+struct Artist<T> {
     drawyer: Drawyer<T>,
+    seed: [u8; 32],
+    pixel: Rgb<u8>,
 }
 
 enum Decision {
@@ -141,17 +167,32 @@ enum Decision {
 }
 
 impl Artist<Rgb<u8>> {
-    fn new(width: u32, height: u32) -> Self {
+    // fn new(width: u64, height: u64, seed: u64) -> Self {
+
+    //     Self {
+    //         drawyer: Drawyer::(width, height, seed),
+    //         seed,
+    //     }
+    // }
+
+    fn with_image(seed: [u8; 32], pixel: Rgb<u8>, image: PixelImage<Rgb<u8>>) -> Self {
+        let mut rng = StdRng::from_seed(seed);
         Self {
-            drawyer: Drawyer::new(width, height),
+            drawyer: Drawyer::with_image(rng, image),
+            seed,
+            pixel,
         }
+    }
+
+    fn rng(&mut self) -> &mut StdRng {
+        self.drawyer.rng()
     }
 
     fn draw(&mut self, pixel: Rgb<u8>) {
         self.drawyer.draw(pixel);
     }
 
-    fn move_cursor(&mut self, x: u32, y: u32) {
+    fn move_cursor(&mut self, x: u64, y: u64) {
         self.drawyer.move_cursor(x, y);
     }
 
@@ -204,16 +245,16 @@ impl Artist<Rgb<u8>> {
         }
     }
 
-    fn decide_direction(&self) -> Decision {
-        
+    fn decide_direction(&mut self) -> Decision {
         let distribution = distributions::WeightedIndex::new(&[
             self.left_probablity(),
             self.right_probablity(),
             self.up_probablity(),
             self.down_probablity(),
-        ]).unwrap();
+        ])
+        .unwrap();
 
-        let mut rng = rand::thread_rng();
+        let mut rng = &mut self.drawyer.rng;
         let decision = match distribution.sample(&mut rng) {
             0 => Decision::Left,
             1 => Decision::Right,
@@ -227,26 +268,65 @@ impl Artist<Rgb<u8>> {
 
     // mirror across x axis like rorchach test
     fn mirror(&mut self) {
-        let mut new_image = PixelImage::new(self.drawyer.image.width, self.drawyer.image.height, None);
+        let mut new_image =
+            PixelImage::new(self.drawyer.image.width, self.drawyer.image.height, None);
         for x in 0..self.drawyer.image.width {
             for y in 0..self.drawyer.image.height {
                 let pixel = self.drawyer.image.get_pixel(x, y).unwrap();
-                // new_image.set_pixel(x, self.drawyer.image.height - y - 1, *pixel);
                 new_image.set_pixel(x, y, *pixel);
                 if x >= self.drawyer.image.width / 2 {
                     let half_image = self.drawyer.image.width / 2;
                     let mirrored_x = x - half_image;
                     let mirrored_x = half_image - mirrored_x;
-                    new_image.set_pixel(mirrored_x , y, *pixel);
+                    new_image.set_pixel(mirrored_x, y, *pixel);
                 }
-                
             }
         }
         self.drawyer.image = new_image;
     }
-    
+
+    fn stamp(&mut self, offset: u64) -> () {
+        let corners = x(self.seed);
+
+        let width = self.drawyer.image.width - 1;
+
+        let side_modifier = [(false, false), (false, true), (true, false), (true, true)];
+
+        for (i, corner) in corners.iter().enumerate() {
+            let a = side_modifier[i].0;
+            let b = side_modifier[i].1;
+            for (x, y) in corner {
+                let x = x + offset;
+                let y = y + offset;
+                let x = if a { x } else { width - x };
+                let y = if b { y } else { width - y };
+                self.move_cursor(x, y);
+                self.drawyer.draw(self.pixel);
+            }
+        }
+    }
+
+    fn draw_random(&mut self) -> () {
+        let steps = 200;
+        let walks = 5;
+
+        for _ in 0..walks {
+            self.drawyer.random_cursor();
+            self.drawyer.draw(self.pixel);
+            for _ in 0..steps {
+                let direction = self.decide_direction();
+                self.move_cursor_by_decision(direction);
+                self.drawyer.draw(self.pixel);
+            }
+        }
+
+        self.mirror();
+    }
 }
 
+fn _is_nth_bit_set(num: u64, n: u64) -> bool {
+    (num & (1 << n)) != 0
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -266,21 +346,26 @@ struct Cli {
     color: RgbX,
 
     #[arg(short, long, default_value = "200")]
-    steps: u32,
+    steps: u64,
 
     #[arg(short, long, default_value = "5")]
-    walks: u32,
+    walks: u64,
+
+    #[arg(long)]
+    seed: Option<u64>,
 }
 
 #[derive(Debug, Clone)]
 struct RgbX(u8, u8, u8);
 
-
 impl FromStr for RgbX {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts = s.split(',').map(|s| s.parse().expect("xxx")).collect::<Vec<u8>>();
+        let parts = s
+            .split(',')
+            .map(|s| s.parse().expect("xxx"))
+            .collect::<Vec<u8>>();
         if parts.len() < 3 {
             Err("kek".to_string())
         } else {
@@ -295,58 +380,129 @@ impl RgbX {
     }
 }
 
+fn u64_to_bool_list(mut num: u64) -> Vec<bool> {
+    let mut bits = Vec::with_capacity(64);
+    for _ in 0..64 {
+        bits.push(num & 1 == 1);
+        num >>= 1;
+    }
+    bits
+}
+
+fn transpose(data: [u8; 32]) -> [u64; 4] {
+    let mut result = [0u64; 4];
+
+    for i in 0..4 {
+        let offset = i * 8;
+        result[i] = ((data[offset] as u64) << 56)
+            | ((data[offset + 1] as u64) << 48)
+            | ((data[offset + 2] as u64) << 40)
+            | ((data[offset + 3] as u64) << 32)
+            | ((data[offset + 4] as u64) << 24)
+            | ((data[offset + 5] as u64) << 16)
+            | ((data[offset + 6] as u64) << 8)
+            | (data[offset + 7] as u64);
+    }
+
+    result
+}
+
+fn reverse_transpose(data: [u64; 4]) -> [u8; 32] {
+    let mut result = [0u8; 32];
+
+    for i in 0..4 {
+        let offset = i * 8;
+        result[offset]     = (data[i] >> 56) as u8;
+        result[offset + 1] = (data[i] >> 48) as u8;
+        result[offset + 2] = (data[i] >> 40) as u8;
+        result[offset + 3] = (data[i] >> 32) as u8;
+        result[offset + 4] = (data[i] >> 24) as u8;
+        result[offset + 5] = (data[i] >> 16) as u8;
+        result[offset + 6] = (data[i] >> 8) as u8;
+        result[offset + 7] = data[i] as u8;
+    }
+
+    result
+}
+
+fn draw(val: u64) -> Vec<(u64, u64)> {
+    let mut v = Vec::new();
+    let mut p = 0u64;
+    let mut x = 0u64;
+    let mut y = 0u64;
+    let mut clockwise: bool = true;
+
+    for b in u64_to_bool_list(val).into_iter() {
+        if b {
+            v.push((x, y));
+        }
+
+        if (x == 0 && y == p && clockwise) || (x == p && y == 0 && !clockwise) {
+            p += 1;
+            clockwise = !clockwise;
+            if x == 0 {
+                y = p
+            } else {
+                x = p
+            }
+        } else if y == x {
+            if clockwise {
+                x -= 1;
+            } else {
+                y -= 1;
+            }
+        } else if x == p {
+            if clockwise {
+                y += 1;
+            } else {
+                y -= 1;
+            }
+        } else if y == p {
+            if clockwise {
+                x -= 1;
+            } else {
+                x += 1;
+            }
+        }
+    }
+    v
+}
+
+fn x(val: [u8; 32]) -> [Vec<(u64, u64)>; 4] {
+    let mut corners: [Vec<(u64, u64)>; 4] = [vec![], vec![], vec![], vec![], ];
+    let val = transpose(val);
+
+    corners[0] = draw(val[0]);
+    corners[1] = draw(val[1]);
+    corners[2] = draw(val[2]);
+    corners[3] = draw(val[3]);
+    corners
+}
+
 fn main() {
-
-    // let mut i: PixelImage<Rgb<u8>> = PixelImage::new(64, 64, Some(Rgb([255, 255, 255])));
-
-    // i.set_pixel(32, 32, Rgb((0, 0, 0).into()));
-    // i.export_image().save("test1.png").unwrap();
-    // define image 64x64
-    // let mut img:ImageBuffer<image::Rgb<u8>, Vec<_>> = image::ImageBuffer::new(64, 64);
-
-
-
-    // // define color black and white
-    // let black = image::Rgb([0, 0, 0]);
-    // let white = image::Rgb([255, 255, 255]);
-
-    // let mut rng = rand::thread_rng();
-
     let cli = Cli::parse();
-
-    // let mut artist: Artist<image::Rgb<u8>> = Artist::new(64, 64);
-
-    let mut artist = Artist {
-        drawyer: Drawyer { cursor_x: 0, cursor_y: 0, image: PixelImage::new(64, 64, Some(cli.background.to_rgb())) }
+    
+    let seed: [u64; 4] = if let Some(seed) = cli.seed {
+        [seed,seed,seed,seed]
+    } else {
+        rand::thread_rng().gen()
     };
+
+    let rng_seed = reverse_transpose(seed);
 
     let pixel = cli.color.to_rgb();
 
-    
-    let steps = 200;
-    let walks = 5;
+    let mut artist = Artist::with_image(
+        rng_seed,
+        pixel,
+        PixelImage::new(64, 64, Some(cli.background.to_rgb())),
+    );
 
-    for _ in 0..walks {
-        artist.drawyer.random_cursor();
-        artist.drawyer.draw(pixel);
-        for _ in 0..steps {
-            let direction = artist.decide_direction();
-            artist.move_cursor_by_decision(direction);
-            artist.drawyer.draw(pixel);
-        }
-    }
+    artist.draw_random();
 
-    artist.mirror();
+    artist.stamp(4);
 
-    let image = artist.drawyer.image;
-    image.export_image().save("./test.png").unwrap();
-    
+    let image = &artist.drawyer.image;
+
     image.upscale(8).export_image().save(cli.output).unwrap();
-
 }
-
-
-
-// fn main() {
-    
-// }
